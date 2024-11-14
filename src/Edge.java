e479f5e15e72eb46e8fc4f0b5129a50e26fcfbf6
blayne-wesneski <102747184+blayne-wesneski@users.noreply.github.com>
2024-11-14 17:51:59 -0500

    Implement Edge class

---
+++ src/Edge.java
@@ -0,0 +1,16 @@
+public class Edge {
+    public int weight;
+    public Node start;
+    public Node end;
+
+    public Edge(Node start, Node end, int weight){
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString(){
+        return this.start.name + " -" + this.weight + " ->" + this.end.name;
+    }
+}
