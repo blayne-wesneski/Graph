19b36430952eb6b11311f63c86b392752e102100
blayne-wesneski <102747184+blayne-wesneski@users.noreply.github.com>
2024-11-14 18:00:22 -0500

    Implement Graph class

--- src/Graph.java
+++ src/Graph.java
@@ -1,5 +1,39 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.LinkedList;
+
+public class Graph {
+    private LinkedList<Node> nodes;
+
+    public Graph() {
+        nodes = new LinkedList<>();
+    }
+
+    /**
+     * Add a vertext to the graph
+     * @param v
+     * @return
+     */
+    public boolean addNode(Node v) {
+        return nodes.add(v);
+    }
+
+    /**
+     * Add an edge to the graph
+     * @param from
+     * @param to
+     * @param weight
+     * @return
+     */
+    public boolean addEdge(Node from, Node to, int weight){
+        return from.edges.add(new Edge(from, to, weight));
+    }
+
+    public void print(){
+        for(Node n : nodes){
+            System.out.print(n + " -> ");
+            for(Edge e : n.edges){
+                System.out.print(e);
+            }
+            System.out.println();
+        }
     }
 }
